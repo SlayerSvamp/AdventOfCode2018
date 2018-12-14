48ae64bcea66434aadcb9760f2c2017920b37ecf
Linus Aspelin <linus.aspelin@trafikverket.se>
2018-12-14 16:58:33 +0100

    day 13 - done

--- 13.py
+++ 13.py
@@ -1,66 +1,59 @@
 import aoc
-from itertools import count
+
+
+class Cart():
+    DIR = dict([(0, (0, -1)), (1, (1, 0)), (2, (0, 1)), (3, (-1, 0))])
+
+    def __init__(self, pos, arrow):
+        self.pos = pos
+        self.facing = '^>v<'.find(arrow)
+        self.turned = 0
+
+    def move(self):
+        (x, y) = self.pos
+        dx, dy = self.DIR[self.facing]
+        self.pos = (x+dx, y+dy)
+
+    def turn(self, track):
+        if track in '\\/+':
+            if track in '\\/':
+                if (self.facing % 2) ^ (track == '/'):
+                    self.facing += 1
+                else:
+                    self.facing += 3
+            elif track == '+':
+                self.facing += [3, 0, 1][self.turned]
+                self.turned = (self.turned+1) % 3
+            self.facing %= 4
+
+    def crashed(self, carts):
+        return next((c for c in carts if c.pos == cart.pos and c != self), None)
+
+    def __repr__(self, *args):
+        return ', '.join([str(self.pos), str(self.facing), str(self.turned)])
+
+
 system = {}
+carts = []
 for y, row in enumerate(aoc.lines()):
     for x, cell in enumerate(row):
         system[x, y] = cell
+        if cell in '<>^v':
+            carts.append(Cart((x, y), cell))
 
-DIR = dict([(0, (0, -1)), (1, (1, 0)), (2, (0, 1)), (3, (-1, 0))])
-carts = [(key, '^>v<'.find(value), 0) for key, value in system.items() if value in '<>v^']
+first_crash_at = None
+while len(carts) > 1:
+    for cart in sorted(carts, key=lambda x: x.pos[::-1]):
+        if cart in carts:
+            cart.move()
+            cart.turn(system[cart.pos])
 
-first_crash = None
-
-for tick in count():
-    if len(carts) == 1:
-        break
-    i = 0
-    while i < len(carts):
-        (x,y), facing, turned = carts[i]
-        dx, dy = DIR[facing]
-        pos = (x+dx, y+dy)
-        track = system[pos]
-        if track in '\\/+':
-            if track in '\\/':
-                if (facing % 2) ^ (track == '/'):
-                    facing += 1
-                else:
-                    facing += 3
-            elif track == '+':
-                facing += [3, 0, 1][turned % 3]
-                turned += 1
-                turned %= 3
-            facing %= 4
-            
-        carts[i] = pos, facing, turned
-        crash_carts = [cart for cart in carts if [c for c, *_ in carts].count(cart[0]) > 1]
-        if crash_carts:
-            if first_crash == None:
-                first_crash = crash_carts[0][0]
-            diff = 0
-            for cart in crash_carts:
-                if carts.index(cart) <= i:
-                    diff -= 1
+            crashed_cart = cart.crashed(carts)
+            if crashed_cart:
+                if not first_crash_at:
+                    first_crash_at = cart.pos
+                carts.remove(crashed_cart)
                 carts.remove(cart)
-            i += diff
-        i += 1
 
-    # import re
-    # for y in range(150):
-    #     row = ''
-    #     for x in range(150):
-    #         if (x,y) not in [c for c, *_ in carts]:
-    #             row += re.sub(r'[<^>v]', '.', aoc.lines()[y][x])
-    #         else:
-
-    #             row += next('^>v<'[d] for c, d, _ in carts if c == (x,y))
-    #     print(row)
-
-    print(carts)
-
-
-print('Part One:', ','.join(map(str, first_crash)))
-print('Part Two:', ','.join(map(str, carts[0][0]))) 
-# not 
-# 19,28 
-# 129,24 
-# 12,83
+print('Part One:', ','.join(map(str, first_crash_at)))
+print('Part Two:', ','.join(map(str, carts[0].pos)))
